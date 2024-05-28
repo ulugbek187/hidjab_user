ec8283794fd41ffb29c84f3086b597742037064b
ulugbek <ulugbekmirvokhidov@gmail.com>
2024-05-28 16:46:53 +0300

    hп

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -4,10 +4,12 @@ import 'package:hidjab_user/bloc/category/category_bloc.dart';
 import 'package:hidjab_user/bloc/category/category_event.dart';
 import 'package:hidjab_user/bloc/nabi/nabi_bloc.dart';
 import 'package:hidjab_user/bloc/nabi/nabi_event.dart';
+import 'package:hidjab_user/data/repo/storage_repository.dart';
 import 'package:hidjab_user/screens/routes.dart';
 import 'package:hidjab_user/utils/styles/size.dart';
 import 'package:lottie/lottie.dart';
-
+import '../../bloc/auth/auth_bloc.dart';
+import '../../data/form_status/form_status.dart';
 import '../../utils/image/appimage.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -18,38 +20,39 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  _init() {
-    Future.microtask((){
+  _init(bool auth) async {
+    Future.microtask(() {
       context.read<CategoryBloc>().add(
-        GetCategories(),
-      );
+            GetCategories(),
+          );
       context.read<NabiBloc>().add(
-        GetCategoryEvent(
-          categories: context.read<CategoryBloc>().state.categories,
-        ),
-      );
+            GetCategoryEvent(
+              categories: context.read<CategoryBloc>().state.categories,
+            ),
+          );
       context.read<NabiBloc>().add(
-        const GetCategoryProductsEvent(),
-      );
+            const GetCategoryProductsEvent(),
+          );
     });
-    Future.delayed(
-      const Duration(
-        seconds: 3,
-      ),
-      () {
-        Navigator.pushReplacementNamed(
-          context,
-          RouteNames.homeScreen,
-        );
-      },
-    );
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+    if (!auth) {
+      bool isNewUser = StorageRepository.getBool(key: "is_new_user");
+      if (isNewUser) {
+        Navigator.pushReplacementNamed(context, RouteNames.login);
+      } else {
+        // Navigator.pushReplacementNamed(context, RouteNames.onBoarding);
+      }
+    } else {
+      Navigator.pushReplacementNamed(context, RouteNames.homeScreen);
+    }
   }
 
-  @override
-  void initState() {
-    _init();
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   _init();
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -58,8 +61,24 @@ class _SplashScreenState extends State<SplashScreen> {
 
     return Scaffold(
       body: Center(
-        child: Lottie.asset(
-          AppImages.eComerse,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocListener<AuthBloc, AuthState>(
+              listener: (context, state) {
+                if (state.status == FormsStatus.authenticated) {
+                  _init(true);
+                } else {
+                  _init(false);
+                }
+              },
+              child: const SizedBox.square(),
+            ),
+            Lottie.asset(
+              AppImages.eComerse,
+            ),
+          ],
         ),
       ),
     );
