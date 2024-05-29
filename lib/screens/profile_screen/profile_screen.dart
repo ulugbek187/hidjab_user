0f9f84791d3d936182f58b5a801578b7e0e24035
ulugbek <ulugbekmirvokhidov@gmail.com>
2024-05-29 16:11:56 +0300

    ава

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hidjab_user/bloc/user/user_bloc.dart';
+import 'package:hidjab_user/data/form_status/form_status.dart';
 import 'package:hidjab_user/utils/styles/app_text_style.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -56,74 +57,91 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
           child: BlocBuilder<UserBloc, UserState>(
             builder: (context, state) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  state.userModel.imageUrl.isEmpty
-                      ? Center(
-                          child: Container(
-                            margin: EdgeInsets.only(
-                              bottom: 30.h,
+              if (state.formsStatus == FormsStatus.error) {
+                return Center(
+                  child: Text(state.errorText),
+                );
+              }
+
+              if (state.formsStatus == FormsStatus.loading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              if (state.formsStatus == FormsStatus.success) {
+                debugPrint("CURRENT USER IS IMAGE: ${state.userModel.imageUrl}");
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    state.userModel.imageUrl.isEmpty
+                        ? Center(
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                bottom: 30.h,
+                              ),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  50.r,
+                                ),
+                                border: Border.all(
+                                  color: Colors.yellowAccent,
+                                  width: 2.w,
+                                ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(
+                                  50.r,
+                                ),
+                                child: Image.network(
+                                  state.userModel.imageUrl,
+                                  width: 150.w,
+                                  height: 150.h,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                50.r,
+                          )
+                        : Center(
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                bottom: 30.h,
                               ),
-                              border: Border.all(
-                                color: Colors.yellowAccent,
-                                width: 2.w,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  50.r,
+                                ),
+                                border: Border.all(
+                                  color: Colors.yellowAccent,
+                                  width: 2.w,
+                                ),
                               ),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                50.r,
-                              ),
-                              child: Image.network(
-                                state.userModel.imageUrl,
-                                width: 150.w,
-                                height: 150.h,
-                                fit: BoxFit.cover,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(
+                                  50.r,
+                                ),
+                                child: Image.network(
+                                  "https://www.tenforums.com/attachments/tutorials/146359d1501443008-change-default-account-picture-windows-10-a-user.png",
+                                  width: 150.w,
+                                  height: 150.h,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
                           ),
-                        )
-                      : Center(
-                          child: Container(
-                            margin: EdgeInsets.only(
-                              bottom: 30.h,
-                            ),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                50.r,
-                              ),
-                              border: Border.all(
-                                color: Colors.yellowAccent,
-                                width: 2.w,
-                              ),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                50.r,
-                              ),
-                              child: Image.network(
-                                "https://www.tenforums.com/attachments/tutorials/146359d1501443008-change-default-account-picture-windows-10-a-user.png",
-                                width: 150.w,
-                                height: 150.h,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                  // ...List.generate(
-                  //   models.length,
-                  //       (index) =>
-                  //       UpdateTextField(
-                  //         title: models[index].title,
-                  //         subTitle: models[index].subTitle,
-                  //       ),
-                  // ),
-                ],
-              );
+                    // ...List.generate(
+                    //   models.length,
+                    //       (index) =>
+                    //       UpdateTextField(
+                    //         title: models[index].title,
+                    //         subTitle: models[index].subTitle,
+                    //       ),
+                    // ),
+                  ],
+                );
+              }
+
+              return const SizedBox.shrink();
             },
           ),
         ),
